a7af2b3528da8ab9cc1f6e6c4fda5925583c9693
Wafflesik <ondrakolmanek@gmail.com>
2026-05-04 14:12:47 +0200

    Update beeper.h

--- beeper.h
+++ beeper.h
@@ -6,5 +6,15 @@ class Beeper
 private:
    int pin;
    int beep_interval;
-   unsigned long
+   unsigned long starttime;
+   unsigned long length;
+   unsigned long pause;
+   int repeat;
+   void startBeep();
+   void stopBeep();
+public:
+   Beeper(pin);
+   void beep(length, pause, repeat);
+   bool checkState();
 }
+#endif
